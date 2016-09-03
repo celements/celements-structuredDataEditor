de59630128f092a734c5d68dee67b92d53fa3931
Fabian Pichler <fabian.pichler@synventis.com>
2016-09-03 20:33:13 +0200

    add StructDataFieldEdit.vm

--- component/src/main/java/com/celements/structEditor/classes/StructEditorClass.java
+++ component/src/main/java/com/celements/structEditor/classes/StructEditorClass.java
@@ -1,7 +1,10 @@
 package com.celements.structEditor.classes;
 
+import org.xwiki.component.annotation.ComponentRole;
+
 import com.celements.model.classes.ClassDefinition;
 
+@ComponentRole
 public interface StructEditorClass extends ClassDefinition {
 
 }
