387f1e98b3535f7800b341a9d8c8c24595e5dca6
Dari Khasham <dari.khasham@synventis.com>
2016-09-27 10:44:31 +0200

    Implemented HiddenTag for form-editor

--- component/src/main/java/com/celements/structEditor/classes/HiddenTagEditorClass.java
+++ component/src/main/java/com/celements/structEditor/classes/HiddenTagEditorClass.java
@@ -7,7 +7,6 @@ import org.xwiki.model.EntityType;
 import org.xwiki.model.reference.EntityReference;
 
 import com.celements.model.classes.AbstractClassDefinition;
-import com.celements.model.classes.fields.BooleanField;
 import com.celements.model.classes.fields.ClassField;
 import com.celements.model.classes.fields.StringField;
 
@@ -16,16 +15,14 @@ import com.celements.model.classes.fields.StringField;
 public class HiddenTagEditorClass extends AbstractClassDefinition implements StructEditorClass {
 
   public static final String SPACE_NAME = "Celements";
-  public static final String DOC_NAME = "SelectTagEditorClass";
+  public static final String DOC_NAME = "HiddenTagEditorClass";
   public static final String CLASS_FN = SPACE_NAME + "." + DOC_NAME;
   public static final String CLASS_DEF_HINT = CLASS_FN;
 
-  public static final ClassField<String> SELECT_TAG_SEPARATOR = new StringField.Builder(
-      CLASS_DEF_HINT, "select_tag_separator").prettyName("Is Separator").build();
-  public static final ClassField<Boolean> SELECT_TAG_IS_BOOTSTRAP = new BooleanField.Builder(
-      CLASS_DEF_HINT, "select_tag_is_bootstrap").prettyName("Is Bootstrap").build();
-  public static final ClassField<Boolean> SELECT_TAG_IS_MULTISELECT = new BooleanField.Builder(
-      CLASS_DEF_HINT, "select_tag_is_multiselect").prettyName("Multiselect").build();
+  public static final ClassField<String> HIDDEN_TAG_NAME = new StringField.Builder(CLASS_DEF_HINT,
+      "hidden_tag_name").prettyName("name").build();
+  public static final ClassField<String> HIDDEN_TAG_VALUE = new StringField.Builder(CLASS_DEF_HINT,
+      "hidden_tag_value").prettyName("value").build();
 
   @Override
   public String getName() {
--- component/src/main/java/com/celements/structEditor/fields/HiddenTagPageType.java
+++ component/src/main/java/com/celements/structEditor/fields/HiddenTagPageType.java
@@ -1,13 +1,33 @@
 package com.celements.structEditor.fields;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
+import org.xwiki.component.annotation.Requirement;
+import org.xwiki.model.reference.DocumentReference;
+
+import com.celements.cells.attribute.AttributeBuilder;
+import com.celements.model.access.exception.DocumentNotExistsException;
+import com.celements.structEditor.classes.HiddenTagEditorClass;
+import com.celements.structEditor.classes.StructEditorClass;
+import com.celements.structEditor.classes.StructuredDataEditorClasses;
+import com.google.common.base.Optional;
+import com.xpn.xwiki.objects.BaseObject;
 
 @Component(HiddenTagPageType.INPUT_FIELD_PAGETYPE_NAME)
 public class HiddenTagPageType extends AbstractStructFieldPageType {
 
+  private static Logger LOGGER = LoggerFactory.getLogger(HiddenTagPageType.class);
+
   public static final String INPUT_FIELD_PAGETYPE_NAME = "HiddenTag";
 
   static final String VIEW_TEMPLATE_NAME = "HiddenTagView";
+
+  @Requirement(HiddenTagEditorClass.CLASS_DEF_HINT)
+  private StructEditorClass hiddenTagEditorClass;
+
+  @Requirement(StructuredDataEditorClasses.CLASS_DEF_HINT)
+  private StructEditorClass structuredDataEditorClasses;
 
   @Override
   public String getName() {
@@ -17,6 +37,29 @@ public class HiddenTagPageType extends AbstractStructFieldPageType {
   @Override
   protected String getViewTemplateName() {
     return VIEW_TEMPLATE_NAME;
+  }
+
+  @Override
+  public Optional<String> defaultTagName() {
+    return Optional.of("input");
+  }
+
+  @Override
+  public void collectAttributes(AttributeBuilder attrBuilder, DocumentReference cellDocRef) {
+    BaseObject hiddenConfig;
+    DocumentReference structuredDataEditorClasseRef = structuredDataEditorClasses.getClassRef(
+        cellDocRef.getWikiReference());
+    DocumentReference hiddenClassRef = hiddenTagEditorClass.getClassRef(
+        cellDocRef.getWikiReference());
+    try {
+      hiddenConfig = modelAccess.getXObject(cellDocRef, hiddenClassRef);
+      attrBuilder.addNonEmptyAttribute("type", "hidden");
+      attrBuilder.addNonEmptyAttribute("name", hiddenConfig.getStringValue("hidden_tag_name"));
+      attrBuilder.addNonEmptyAttribute("value", hiddenConfig.getStringValue("hidden_tag_value"));
+    } catch (DocumentNotExistsException exc) {
+      LOGGER.error("Document {} or Document {} does not exist {}", structuredDataEditorClasseRef,
+          hiddenClassRef, exc);
+    }
   }
 
 }
