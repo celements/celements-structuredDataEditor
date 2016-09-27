876017e33f89a9e389c780ec828820906b3964ae
Dari Khasham <dari.khasham@synventis.com>
2016-09-27 16:43:04 +0200

    Added FormFieldEditorClass and configured its PageType

---
+++ component/src/main/java/com/celements/structEditor/classes/FormFieldEditorClass.java
@@ -0,0 +1,46 @@
+package com.celements.structEditor.classes;
+
+import javax.inject.Singleton;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.model.EntityType;
+import org.xwiki.model.reference.EntityReference;
+
+import com.celements.model.classes.AbstractClassDefinition;
+import com.celements.model.classes.fields.BooleanField;
+import com.celements.model.classes.fields.ClassField;
+import com.celements.model.classes.fields.StringField;
+
+@Singleton
+@Component(FormFieldEditorClass.CLASS_DEF_HINT)
+public class FormFieldEditorClass extends AbstractClassDefinition implements StructEditorClass {
+
+  public static final String SPACE_NAME = "Celements";
+  public static final String DOC_NAME = "FormFieldEditorClass";
+  public static final String CLASS_FN = SPACE_NAME + "." + DOC_NAME;
+  public static final String CLASS_DEF_HINT = CLASS_FN;
+
+  public static final ClassField<String> FORM_FIELD_ACTION = new StringField.Builder(CLASS_DEF_HINT,
+      "form_field_action").prettyName("Action").build();
+  public static final ClassField<String> FORM_FIELD_METHOD = new StringField.Builder(CLASS_DEF_HINT,
+      "form_field_method").prettyName("Method").build();
+  public static final ClassField<Boolean> FORM_FIELD_SEND_DATA_ENCODED = new BooleanField.Builder(
+      CLASS_DEF_HINT, "form_field_send_data_encoded").prettyName("Send Data Encoded").build();
+
+  @Override
+  public String getName() {
+    return CLASS_DEF_HINT;
+  }
+
+  @Override
+  public boolean isInternalMapping() {
+    return true;
+  }
+
+  @Override
+  protected EntityReference getRelativeClassRef() {
+    return new EntityReference(DOC_NAME, EntityType.DOCUMENT, new EntityReference(SPACE_NAME,
+        EntityType.SPACE));
+  }
+
+}
--- component/src/main/java/com/celements/structEditor/fields/FormFieldPageType.java
+++ component/src/main/java/com/celements/structEditor/fields/FormFieldPageType.java
@@ -1,22 +1,34 @@
 package com.celements.structEditor.fields;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.Requirement;
 import org.xwiki.model.reference.DocumentReference;
 
 import com.celements.cells.attribute.AttributeBuilder;
+import com.celements.model.access.exception.DocumentNotExistsException;
 import com.celements.model.context.ModelContext;
+import com.celements.structEditor.classes.FormFieldEditorClass;
+import com.celements.structEditor.classes.StructEditorClass;
 import com.google.common.base.Optional;
+import com.google.common.base.Strings;
+import com.xpn.xwiki.objects.BaseObject;
 
 @Component(FormFieldPageType.FORM_FIELD_PAGETYPE_NAME)
 public class FormFieldPageType extends AbstractStructFieldPageType {
 
+  private static Logger LOGGER = LoggerFactory.getLogger(SelectTagPageType.class);
+
+  @Requirement(FormFieldEditorClass.CLASS_DEF_HINT)
+  private StructEditorClass formFieldEditorClass;
+
+  @Requirement
+  private ModelContext modelContext;
+
   public static final String FORM_FIELD_PAGETYPE_NAME = "FormField";
 
   static final String VIEW_TEMPLATE_NAME = "FormFieldView";
-
-  @Requirement
-  ModelContext modelContext;
 
   @Override
   public String getName() {
@@ -35,8 +47,29 @@ public class FormFieldPageType extends AbstractStructFieldPageType {
 
   @Override
   public void collectAttributes(AttributeBuilder attrBuilder, DocumentReference cellDocRef) {
-    attrBuilder.addNonEmptyAttribute("method", "post");
-    attrBuilder.addNonEmptyAttribute("action", "?");
+    attrBuilder.addCssClasses("celAddValidationToForm inactive");
+    BaseObject formFieldConfig;
+    DocumentReference formFieldClassRef = formFieldEditorClass.getClassRef(
+        cellDocRef.getWikiReference());
+    try {
+      formFieldConfig = modelAccess.getXObject(cellDocRef, formFieldClassRef);
+      if (formFieldConfig.getIntValue("form_field_send_data_encoded") == 1) {
+        attrBuilder.addNonEmptyAttribute("enctype", "multipart/form-data");
+      }
+      String method = new String();
+      if (!Strings.isNullOrEmpty(formFieldConfig.getStringValue("form_field_method"))) {
+        method = formFieldConfig.getStringValue("form_field_method");
+      } else {
+        method = "post";
+      }
+      if (!Strings.isNullOrEmpty(formFieldConfig.getStringValue("form_field_action"))) {
+        attrBuilder.addNonEmptyAttribute("action", formFieldConfig.getStringValue(
+            "form_field_action"));
+      }
+      attrBuilder.addNonEmptyAttribute("method", method);
+    } catch (DocumentNotExistsException exc) {
+      LOGGER.error("Document {} or Document {} does not exist {}", formFieldClassRef, exc);
+    }
   }
 
 }
