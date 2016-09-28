754cdcfe63fda2d4e1c973953ba0c53dbb91db04
Dari Khasham <dari.khasham@synventis.com>
2016-09-28 17:16:04 +0200

    removed select rows/cols on pageType, get it through ScriptService

--- component/src/main/java/com/celements/structEditor/fields/TextAreaFieldPageType.java
+++ component/src/main/java/com/celements/structEditor/fields/TextAreaFieldPageType.java
@@ -1,6 +1,10 @@
 package com.celements.structEditor.fields;
 
 import org.xwiki.component.annotation.Component;
+import org.xwiki.component.annotation.Requirement;
+
+import com.celements.structEditor.classes.StructEditorClass;
+import com.celements.structEditor.classes.TextAreaFieldEditorClass;
 
 @Component(TextAreaFieldPageType.INPUT_FIELD_PAGETYPE_NAME)
 public class TextAreaFieldPageType extends AbstractStructFieldPageType {
@@ -8,6 +12,9 @@ public class TextAreaFieldPageType extends AbstractStructFieldPageType {
   public static final String INPUT_FIELD_PAGETYPE_NAME = "TextAreaField";
 
   static final String VIEW_TEMPLATE_NAME = "TextAreaFieldView";
+
+  @Requirement(TextAreaFieldEditorClass.CLASS_DEF_HINT)
+  private StructEditorClass textAreaFieldEditorClass;
 
   @Override
   public String getName() {
