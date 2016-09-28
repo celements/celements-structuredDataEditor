2cb44ac9b21c55d4974068a3add875e27f788e32
Dari Khasham <dari.khasham@synventis.com>
2016-09-28 09:38:54 +0200

    add OptionTagEditorClass

---
+++ component/src/main/java/com/celements/structEditor/classes/OptionTagEditorClass.java
@@ -0,0 +1,48 @@
+package com.celements.structEditor.classes;
+
+import javax.inject.Singleton;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.model.EntityType;
+import org.xwiki.model.reference.EntityReference;
+
+import com.celements.model.classes.AbstractClassDefinition;
+import com.celements.model.classes.fields.BooleanField;
+import com.celements.model.classes.fields.ClassField;
+import com.celements.model.classes.fields.StringField;
+
+@Singleton
+@Component(OptionTagEditorClass.CLASS_DEF_HINT)
+public class OptionTagEditorClass extends AbstractClassDefinition implements StructEditorClass {
+
+  public static final String SPACE_NAME = "Celements";
+  public static final String DOC_NAME = "OptionTagEditorClass";
+  public static final String CLASS_FN = SPACE_NAME + "." + DOC_NAME;
+  public static final String CLASS_DEF_HINT = CLASS_FN;
+
+  public static final ClassField<String> OPTION_TAG_VALUE = new StringField.Builder(CLASS_DEF_HINT,
+      "option_tag_value").prettyName("value").build();
+  public static final ClassField<String> OPTION_TAG_LABEL = new StringField.Builder(CLASS_DEF_HINT,
+      "option_tag_label").prettyName("label").build();
+  public static final ClassField<Boolean> OPTION_TAG_SELECTED = new BooleanField.Builder(
+      CLASS_DEF_HINT, "option_tag_is_selected").prettyName("Is Selected").build();
+  public static final ClassField<Boolean> OPTION_TAG_DISABLED = new BooleanField.Builder(
+      CLASS_DEF_HINT, "option_tag_is_disabled").prettyName("Is Disabled").build();
+
+  @Override
+  public String getName() {
+    return CLASS_DEF_HINT;
+  }
+
+  @Override
+  public boolean isInternalMapping() {
+    return true;
+  }
+
+  @Override
+  protected EntityReference getRelativeClassRef() {
+    return new EntityReference(DOC_NAME, EntityType.DOCUMENT, new EntityReference(SPACE_NAME,
+        EntityType.SPACE));
+  }
+
+}
