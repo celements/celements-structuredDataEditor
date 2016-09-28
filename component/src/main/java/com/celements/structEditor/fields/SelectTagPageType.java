946e8b9ab925eb9d3061de21193dabe9342b22bf
Dari Khasham <dari.khasham@synventis.com>
2016-09-28 11:45:37 +0200

    implemented Rendering for SelectTag

--- component/src/main/java/com/celements/structEditor/fields/SelectTagPageType.java
+++ component/src/main/java/com/celements/structEditor/fields/SelectTagPageType.java
@@ -1,23 +1,30 @@
 package com.celements.structEditor.fields;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.Requirement;
 import org.xwiki.model.reference.DocumentReference;
 
 import com.celements.cells.attribute.AttributeBuilder;
-import com.celements.model.context.ModelContext;
+import com.celements.model.access.exception.DocumentNotExistsException;
 import com.celements.structEditor.classes.SelectTagEditorClass;
 import com.celements.structEditor.classes.StructEditorClass;
+import com.celements.structEditor.classes.StructuredDataEditorClasses;
+import com.google.common.base.Optional;
+import com.google.common.base.Strings;
 import com.xpn.xwiki.objects.BaseObject;
 
 @Component(SelectTagPageType.SELECT_TAG_PAGETYPE_NAME)
 public class SelectTagPageType extends AbstractStructFieldPageType {
 
+  private static Logger LOGGER = LoggerFactory.getLogger(SelectTagPageType.class);
+
   @Requirement(SelectTagEditorClass.CLASS_DEF_HINT)
   private StructEditorClass selectTagEditorClass;
 
-  @Requirement
-  private ModelContext modelContext;
+  @Requirement(StructuredDataEditorClasses.CLASS_DEF_HINT)
+  private StructEditorClass structuredDataEditorClass;
 
   public static final String SELECT_TAG_PAGETYPE_NAME = "SelectTag";
 
@@ -34,30 +41,39 @@ public class SelectTagPageType extends AbstractStructFieldPageType {
   }
 
   @Override
+  public Optional<String> defaultTagName() {
+    return Optional.of("select");
+  }
+
+  @Override
   public void collectAttributes(AttributeBuilder attrBuilder, DocumentReference cellDocRef) {
+    BaseObject structuredDataEditorConfig;
     BaseObject selectConfig;
-    // try {
-    System.out.println("\n<<<<<<<<<<<<<<<<<<<<<<<< TEST");
-    DocumentReference classRef = selectTagEditorClass.getClassRef(cellDocRef.getWikiReference());
-    // selectConfig = modelAccess.getXObject(cellDocRef, classRef);
-    // System.out.println(
-    // "\n<<<<<<<<<<<<<<<<<<<<<<<< SelectTagPageType collectAttributes selectConfig: ["
-    // + selectConfig + "]\n" + "\n<<<<<<<<<<<<<<<<<<<<<<<< cellDocRef: [" + cellDocRef
-    // + "]\n" + "\n<<<<<<<<<<<<<<<<<<<<<<<< selectTagEditorClass.getClassRef(): ["
-    // + selectTagEditorClass.getClassRef(cellDocRef.getWikiReference()) + "]\n"
-    // + "selectConfig.getIntValue('select_tag_is_multiselect'): ["
-    // + selectConfig.getIntValue("select_tag_is_multiselect") + "]");
-    //
-    // if (selectConfig.getIntValue("select_tag_is_multiselect") == 1) {
-    // attrBuilder.addCssClasses("celMultiselect");
-    // }
-    // attrBuilder.addNonEmptyAttribute("selected", "");
-    // attrBuilder.addNonEmptyAttribute("name", modelAccess.getXObject(
-    // modelContext.getDoc().getDocumentReference(), get));
-    // } catch (DocumentNotExistsException exp) {
-    // // TODO Auto-generated catch block
-    // exp.printStackTrace();
-    // }
+    DocumentReference structuredDataEditorClassRef = structuredDataEditorClass.getClassRef(
+        cellDocRef.getWikiReference());
+    DocumentReference selectClassRef = selectTagEditorClass.getClassRef(
+        cellDocRef.getWikiReference());
+    try {
+      structuredDataEditorConfig = modelAccess.getXObject(cellDocRef, structuredDataEditorClassRef);
+      selectConfig = modelAccess.getXObject(cellDocRef, selectClassRef);
+
+      if (selectConfig.getIntValue("select_tag_is_multiselect") == 1) {
+        attrBuilder.addCssClasses("celMultiselect");
+      }
+      if (selectConfig.getIntValue("select_tag_is_bootstrap") == 1) {
+        attrBuilder.addCssClasses("celBootstrap");
+      }
+      if (!Strings.isNullOrEmpty(structuredDataEditorConfig.getStringValue(
+          "select_tag_separator"))) {
+
+      }
+      attrBuilder.addNonEmptyAttribute("name", structuredDataEditorConfig.getStringValue(
+          "edit_field_class_fullname") + "_0_" + structuredDataEditorConfig.getStringValue(
+              "edit_field_name"));
+    } catch (DocumentNotExistsException exc) {
+      LOGGER.error("Document {} or Document {} does not exist {}", structuredDataEditorClassRef,
+          selectClassRef, exc);
+    }
   }
 
 }
