8bfa774accc3a9eb92df6d6a63e23f1625d9da67
Marc Sladek <marc.sladek@synventis.com>
2016-09-29 15:17:16 +0200

    refactored usages of fields

--- component/src/main/java/com/celements/structEditor/fields/OptionTagPageType.java
+++ component/src/main/java/com/celements/structEditor/fields/OptionTagPageType.java
@@ -1,25 +1,21 @@
 package com.celements.structEditor.fields;
 
+import static com.celements.structEditor.classes.OptionTagEditorClass.*;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
-import org.xwiki.component.annotation.Requirement;
 import org.xwiki.model.reference.DocumentReference;
 
 import com.celements.cells.attribute.AttributeBuilder;
 import com.celements.model.access.exception.DocumentNotExistsException;
-import com.celements.structEditor.classes.OptionTagEditorClass;
-import com.celements.structEditor.classes.StructEditorClass;
 import com.google.common.base.Optional;
-import com.xpn.xwiki.objects.BaseObject;
+import com.xpn.xwiki.doc.XWikiDocument;
 
 @Component(OptionTagPageType.PAGETYPE_NAME)
 public class OptionTagPageType extends AbstractStructFieldPageType {
 
   private static Logger LOGGER = LoggerFactory.getLogger(OptionTagPageType.class);
-
-  @Requirement(OptionTagEditorClass.CLASS_DEF_HINT)
-  private StructEditorClass optionTagEditorClass;
 
   public static final String PAGETYPE_NAME = "OptionTag";
 
@@ -42,22 +38,18 @@ public class OptionTagPageType extends AbstractStructFieldPageType {
 
   @Override
   public void collectAttributes(AttributeBuilder attrBuilder, DocumentReference cellDocRef) {
-    BaseObject optionConfig;
-    DocumentReference optionClassRef = optionTagEditorClass.getClassRef(
-        cellDocRef.getWikiReference());
     try {
-      optionConfig = modelAccess.getXObject(cellDocRef, optionClassRef);
-
-      if (optionConfig.getIntValue("option_tag_is_selected") == 1) {
+      XWikiDocument cellDoc = modelAccess.getDocument(cellDocRef);
+      if (modelAccess.getProperty(cellDoc, FIELD_SELECTED)) {
         attrBuilder.addEmptyAttribute("selected");
       }
-      if (optionConfig.getIntValue("option_tag_is_disabled") == 1) {
+      if (modelAccess.getProperty(cellDoc, FIELD_DISABLED)) {
         attrBuilder.addEmptyAttribute("disabled");
       }
-      attrBuilder.addNonEmptyAttribute("value", optionConfig.getStringValue("option_tag_value"));
-      attrBuilder.addNonEmptyAttribute("label", optionConfig.getStringValue("option_tag_label"));
+      attrBuilder.addNonEmptyAttribute("value", modelAccess.getProperty(cellDoc, FIELD_VALUE));
+      attrBuilder.addNonEmptyAttribute("label", modelAccess.getProperty(cellDoc, FIELD_LABEL));
     } catch (DocumentNotExistsException exc) {
-      LOGGER.error("Document {} or Document {} does not exist {}", optionClassRef, exc);
+      LOGGER.error("cell doesn't exist '{}'", cellDocRef, exc);
     }
   }
 
