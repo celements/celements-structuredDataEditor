140bf2ed585a7a6b7673b301f560c6794af4a88b
Marc Sladek <marc.sladek@synventis.com>
2016-09-29 14:24:40 +0200

    implemented addNameAttribute

--- component/src/main/java/com/celements/structEditor/fields/AbstractStructFieldPageType.java
+++ component/src/main/java/com/celements/structEditor/fields/AbstractStructFieldPageType.java
@@ -1,13 +1,23 @@
 package com.celements.structEditor.fields;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import org.xwiki.component.annotation.Requirement;
+import org.xwiki.model.reference.DocumentReference;
 
+import com.celements.cells.attribute.AttributeBuilder;
 import com.celements.model.access.IModelAccessFacade;
+import com.celements.model.util.ModelUtils;
 import com.celements.pagetype.category.IPageTypeCategoryRole;
 import com.celements.pagetype.java.AbstractJavaPageType;
+import com.celements.structEditor.classes.StructuredDataEditorClass;
+import com.google.common.base.Joiner;
+import com.google.common.base.Strings;
 import com.google.common.collect.Sets;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
 
 public abstract class AbstractStructFieldPageType extends AbstractJavaPageType {
 
@@ -18,6 +28,9 @@ public abstract class AbstractStructFieldPageType extends AbstractJavaPageType {
 
   @Requirement
   protected IModelAccessFacade modelAccess;
+
+  @Requirement
+  protected ModelUtils modelUtils;
 
   @Override
   public Set<IPageTypeCategoryRole> getCategories() {
@@ -57,6 +70,30 @@ public abstract class AbstractStructFieldPageType extends AbstractJavaPageType {
     } else {
       return getViewTemplateName();
     }
+  }
+
+  /**
+   * used for tags with name attribute
+   */
+  protected final void addNameAttribute(AttributeBuilder attrBuilder, XWikiDocument cellDoc) {
+    List<String> nameParts = new ArrayList<>();
+    String className = modelAccess.getProperty(cellDoc,
+        StructuredDataEditorClass.FIELD_EDIT_FIELD_CLASS_NAME);
+    if (!className.isEmpty()) {
+      int objNb = getObjNumber(cellDoc, className);
+      nameParts.add(Strings.emptyToNull(className));
+      nameParts.add(Strings.emptyToNull(Integer.toString(objNb)));
+    }
+    nameParts.add(Strings.emptyToNull(modelAccess.getProperty(cellDoc,
+        StructuredDataEditorClass.FIELD_EDIT_FIELD_NAME)));
+    attrBuilder.addAttribute("name", Joiner.on('_').skipNulls().join(nameParts));
+  }
+
+  private int getObjNumber(XWikiDocument cellDoc, String className) {
+    DocumentReference classRef = modelUtils.resolveRef(className, DocumentReference.class,
+        cellDoc.getDocumentReference().getWikiReference());
+    BaseObject obj = modelAccess.getXObject(cellDoc, classRef);
+    return obj != null ? obj.getNumber() : -1;
   }
 
 }
