79b41d27018913f9af407a1530bc272c0c68c82a
Fabian Pichler <fabian.pichler@synventis.com>
2016-09-30 21:47:54 +0200

    fix test_getXClassPrettyName unit test

--- component/src/test/java/com/celements/structEditor/DefaultStructuredDataEditorServiceTest.java
+++ component/src/test/java/com/celements/structEditor/DefaultStructuredDataEditorServiceTest.java
@@ -16,21 +16,30 @@ import com.celements.model.access.IModelAccessFacade;
 import com.celements.pagetype.PageTypeReference;
 import com.celements.pagetype.service.IPageTypeResolverRole;
 import com.celements.structEditor.classes.FormFieldEditorClass;
+import com.celements.structEditor.classes.StructuredDataEditorClass;
 import com.celements.structEditor.fields.FormFieldPageType;
+import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.classes.BaseClass;
 import com.xpn.xwiki.web.Utils;
 
 public class DefaultStructuredDataEditorServiceTest extends AbstractComponentTest {
 
   private DefaultStructuredDataEditorService service;
   private XWikiDocument cellDoc;
+  private IModelAccessFacade modelAccessMock;
+  private XWikiContext context;
+  private String wikiName;
 
   @Before
   public void prepareTest() throws Exception {
     registerComponentMocks(IModelAccessFacade.class, IPageTypeResolverRole.class);
     service = (DefaultStructuredDataEditorService) Utils.getComponent(
         StructuredDataEditorService.class);
-    cellDoc = new XWikiDocument(new DocumentReference("wiki", "layout", "cell"));
+    modelAccessMock = getMock(IModelAccessFacade.class);
+    context = getContext();
+    wikiName = context.getDatabase();
+    cellDoc = new XWikiDocument(new DocumentReference(wikiName, "layout", "cell"));
   }
 
   @Test
@@ -45,14 +54,15 @@ public class DefaultStructuredDataEditorServiceTest extends AbstractComponentTes
   @Test
   public void test_resolveFormPrefix() throws Exception {
     String prefix = "prefix";
-    XWikiDocument parentDoc = new XWikiDocument(new DocumentReference("wiki", "layout", "parent"));
+    XWikiDocument parentDoc = new XWikiDocument(new DocumentReference(wikiName, "layout",
+        "parent"));
     cellDoc.setParentReference((EntityReference) parentDoc.getDocumentReference());
-    expect(getMock(IModelAccessFacade.class).getDocument(eq(
-        parentDoc.getDocumentReference()))).andReturn(parentDoc).once();
+    expect(modelAccessMock.getDocument(eq(parentDoc.getDocumentReference()))).andReturn(
+        parentDoc).once();
     expect(getMock(IPageTypeResolverRole.class).getPageTypeRefForDoc(same(parentDoc))).andReturn(
         new PageTypeReference(FormFieldPageType.PAGETYPE_NAME, "",
             Collections.<String>emptyList())).once();
-    expect(getMock(IModelAccessFacade.class).getProperty(same(parentDoc), same(
+    expect(modelAccessMock.getProperty(same(parentDoc), same(
         FormFieldEditorClass.FIELD_PREFIX))).andReturn(prefix).once();
     replayDefault();
     String ret = service.resolveFormPrefix(cellDoc);
@@ -62,11 +72,22 @@ public class DefaultStructuredDataEditorServiceTest extends AbstractComponentTes
 
   @Test
   public void test_getXClassPrettyName() throws Exception {
-    String prettyName = "";
+    String fieldXClassName = "Celements.TestXClassName";
+    expect(modelAccessMock.getProperty(same(cellDoc), same(
+        StructuredDataEditorClass.FIELD_EDIT_FIELD_CLASS_NAME))).andReturn(fieldXClassName).once();
+    DocumentReference xClassDocRef = new DocumentReference(wikiName, "Celements", "TestXClassName");
+    XWikiDocument xClassDoc = new XWikiDocument(xClassDocRef);
+    expect(modelAccessMock.getDocument(xClassDocRef)).andReturn(xClassDoc).atLeastOnce();
+    String editFieldName = "edit_field";
+    expect(modelAccessMock.getProperty(same(cellDoc), same(
+        StructuredDataEditorClass.FIELD_EDIT_FIELD_NAME))).andReturn(editFieldName).once();
+    BaseClass xClass = xClassDoc.getXClass();
+    String thePrettyFieldName = "the Pretty Field Name";
+    xClass.addTextField(editFieldName, thePrettyFieldName, 30);
     replayDefault();
     String ret = service.getXClassPrettyName(cellDoc);
     verifyDefault();
-    assertEquals(prettyName, ret);
+    assertEquals(thePrettyFieldName, ret);
   }
 
 }
