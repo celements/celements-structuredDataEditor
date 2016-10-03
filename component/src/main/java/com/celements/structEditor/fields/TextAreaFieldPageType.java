1fee2629050015888778d4f009ce6f5f314cb0b1
Marc Sladek <marc.sladek@synventis.com>
2016-10-03 16:02:10 +0200

    TextField/TextAreaField accessing attributes via ScriptService

--- component/src/main/java/com/celements/structEditor/fields/TextAreaFieldPageType.java
+++ component/src/main/java/com/celements/structEditor/fields/TextAreaFieldPageType.java
@@ -1,21 +1,9 @@
 package com.celements.structEditor.fields;
 
-import static com.celements.structEditor.classes.TextAreaFieldEditorClass.*;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
-import org.xwiki.model.reference.DocumentReference;
-
-import com.celements.cells.attribute.AttributeBuilder;
-import com.celements.model.access.exception.DocumentNotExistsException;
-import com.google.common.base.Optional;
-import com.xpn.xwiki.doc.XWikiDocument;
 
 @Component(TextAreaFieldPageType.INPUT_FIELD_PAGETYPE_NAME)
 public class TextAreaFieldPageType extends AbstractStructFieldPageType {
-
-  private static Logger LOGGER = LoggerFactory.getLogger(TextAreaFieldPageType.class);
 
   public static final String INPUT_FIELD_PAGETYPE_NAME = "TextAreaField";
 
@@ -29,25 +17,6 @@ public class TextAreaFieldPageType extends AbstractStructFieldPageType {
   @Override
   protected String getViewTemplateName() {
     return VIEW_TEMPLATE_NAME;
-  }
-
-  @Override
-  public Optional<String> defaultTagName() {
-    return Optional.of("textarea");
-  }
-
-  @Override
-  public void collectAttributes(AttributeBuilder attrBuilder, DocumentReference cellDocRef) {
-    try {
-      XWikiDocument cellDoc = modelAccess.getDocument(cellDocRef);
-      addNameAttribute(attrBuilder, cellDoc);
-      attrBuilder.addNonEmptyAttribute("cols", modelAccess.getProperty(cellDoc,
-          FIELD_COLS).toString());
-      attrBuilder.addNonEmptyAttribute("rows", modelAccess.getProperty(cellDoc,
-          FIELD_ROWS).toString());
-    } catch (DocumentNotExistsException exc) {
-      LOGGER.error("cell doesn't exist '{}'", cellDocRef, exc);
-    }
   }
 
 }
--- component/src/main/java/com/celements/structEditor/fields/TextFieldPageType.java
+++ component/src/main/java/com/celements/structEditor/fields/TextFieldPageType.java
@@ -1,19 +1,9 @@
 package com.celements.structEditor.fields;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
-import org.xwiki.model.reference.DocumentReference;
-
-import com.celements.cells.attribute.AttributeBuilder;
-import com.celements.model.access.exception.DocumentNotExistsException;
-import com.google.common.base.Optional;
-import com.xpn.xwiki.doc.XWikiDocument;
 
 @Component(TextFieldPageType.PAGETYPE_NAME)
 public class TextFieldPageType extends AbstractStructFieldPageType {
-
-  private static Logger LOGGER = LoggerFactory.getLogger(TextFieldPageType.class);
 
   public static final String PAGETYPE_NAME = "InputField";
 
@@ -27,22 +17,6 @@ public class TextFieldPageType extends AbstractStructFieldPageType {
   @Override
   protected String getViewTemplateName() {
     return VIEW_TEMPLATE_NAME;
-  }
-
-  @Override
-  public Optional<String> defaultTagName() {
-    return Optional.of("input");
-  }
-
-  @Override
-  public void collectAttributes(AttributeBuilder attrBuilder, DocumentReference cellDocRef) {
-    try {
-      XWikiDocument cellDoc = modelAccess.getDocument(cellDocRef);
-      attrBuilder.addNonEmptyAttribute("type", "text");
-      addNameAttribute(attrBuilder, cellDoc);
-    } catch (DocumentNotExistsException exc) {
-      LOGGER.error("cell doesn't exist '{}'", cellDocRef, exc);
-    }
   }
 
 }
