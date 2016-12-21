834e8f2d74d3e1d3584ef20a99d72d9c82365e2f
Marc Sladek <marc.sladek@synventis.com>
2016-12-21 15:56:10 +0100

    fixed StructuredDataEditorClasses

--- component/src/main/java/com/celements/structEditor/classes/StructuredDataEditorClasses.java
+++ component/src/main/java/com/celements/structEditor/classes/StructuredDataEditorClasses.java
@@ -3,8 +3,6 @@ package com.celements.structEditor.classes;
 import javax.inject.Singleton;
 
 import org.xwiki.component.annotation.Component;
-import org.xwiki.model.EntityType;
-import org.xwiki.model.reference.EntityReference;
 
 import com.celements.model.classes.AbstractClassDefinition;
 import com.celements.model.classes.fields.ClassField;
@@ -36,9 +34,13 @@ public class StructuredDataEditorClasses extends AbstractClassDefinition impleme
   }
 
   @Override
-  protected EntityReference getRelativeClassRef() {
-    return new EntityReference(DOC_NAME, EntityType.DOCUMENT, new EntityReference(SPACE_NAME,
-        EntityType.SPACE));
+  protected String getClassSpaceName() {
+    return SPACE_NAME;
+  }
+
+  @Override
+  protected String getClassDocName() {
+    return DOC_NAME;
   }
 
 }
