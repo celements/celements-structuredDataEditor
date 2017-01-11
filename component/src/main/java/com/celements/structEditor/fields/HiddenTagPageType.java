5e2a60df188f45acb6727cb77e4c545f5ee3adb3
Marc Sladek <marc.sladek@synventis.com>
2017-01-11 13:56:48 +0100

    added velocity evaluating to HiddenPageType

--- component/src/main/java/com/celements/structEditor/fields/HiddenTagPageType.java
+++ component/src/main/java/com/celements/structEditor/fields/HiddenTagPageType.java
@@ -6,10 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.velocity.XWikiVelocityException;
 
 import com.celements.cells.attribute.AttributeBuilder;
 import com.celements.model.access.exception.DocumentNotExistsException;
 import com.google.common.base.Optional;
+import com.xpn.xwiki.doc.XWikiDocument;
 
 @Component(HiddenTagPageType.PAGETYPE_NAME)
 public class HiddenTagPageType extends AbstractStructFieldPageType {
@@ -39,11 +41,12 @@ public class HiddenTagPageType extends AbstractStructFieldPageType {
   public void collectAttributes(AttributeBuilder attrBuilder, DocumentReference cellDocRef) {
     attrBuilder.addNonEmptyAttribute("type", "hidden");
     try {
-      attrBuilder.addNonEmptyAttribute("name", getNotEmptyString(cellDocRef, FIELD_NAME).or(
+      XWikiDocument cellDoc = modelAccess.getDocument(cellDocRef);
+      attrBuilder.addNonEmptyAttribute("name", modelAccess.getFieldValue(cellDoc, FIELD_NAME).or(
           cellDocRef.getName()));
-      attrBuilder.addNonEmptyAttribute("value", getNotEmptyString(cellDocRef, FIELD_VALUE).or(""));
-    } catch (DocumentNotExistsException exc) {
-      LOGGER.error("cell doesn't exist '{}'", cellDocRef, exc);
+      attrBuilder.addNonEmptyAttribute("value", getVelocityFieldValue(cellDoc, FIELD_VALUE).or(""));
+    } catch (DocumentNotExistsException | XWikiVelocityException exc) {
+      LOGGER.error("failed to add all attributes for '{}'", cellDocRef, exc);
     }
   }
 
