a74b44fe7515423f71a7d2a1e0c03b0fc300cdd5
Marc Sladek <marc.sladek@synventis.com>
2017-01-17 18:21:19 +0100

    SubmitLinkView changed to adminMsg

--- component/src/main/java/com/celements/structEditor/fields/SubmitLinkPageType.java
+++ component/src/main/java/com/celements/structEditor/fields/SubmitLinkPageType.java
@@ -1,12 +1,9 @@
 package com.celements.structEditor.fields;
 
 import org.xwiki.component.annotation.Component;
-import org.xwiki.component.annotation.Requirement;
 import org.xwiki.model.reference.DocumentReference;
 
 import com.celements.cells.attribute.AttributeBuilder;
-import com.celements.structEditor.classes.StructEditorClass;
-import com.celements.structEditor.classes.StructuredDataEditorClass;
 import com.google.common.base.Optional;
 
 @Component(SubmitLinkPageType.PAGETYPE_NAME)
@@ -15,9 +12,6 @@ public class SubmitLinkPageType extends AbstractStructFieldPageType {
   public static final String PAGETYPE_NAME = "SubmitLink";
 
   static final String VIEW_TEMPLATE_NAME = "SubmitLinkView";
-
-  @Requirement(StructuredDataEditorClass.CLASS_DEF_HINT)
-  private StructEditorClass structuredDataEditorClasses;
 
   @Override
   public String getName() {
