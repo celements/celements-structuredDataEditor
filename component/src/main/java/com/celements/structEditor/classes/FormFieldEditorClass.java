6effe2be09b0550f97cc93575abdadba025baf01
Marc Sladek <marc.sladek@synventis.com>
2017-01-31 12:45:55 +0100

    changed Form method to EnumListField

--- component/src/main/java/com/celements/structEditor/classes/FormFieldEditorClass.java
+++ component/src/main/java/com/celements/structEditor/classes/FormFieldEditorClass.java
@@ -1,5 +1,7 @@
 package com.celements.structEditor.classes;
 
+import java.util.List;
+
 import javax.inject.Singleton;
 
 import org.xwiki.component.annotation.Component;
@@ -8,6 +10,7 @@ import com.celements.model.classes.AbstractClassDefinition;
 import com.celements.model.classes.fields.BooleanField;
 import com.celements.model.classes.fields.ClassField;
 import com.celements.model.classes.fields.StringField;
+import com.celements.model.classes.fields.list.EnumListField;
 
 @Singleton
 @Component(FormFieldEditorClass.CLASS_DEF_HINT)
@@ -17,10 +20,15 @@ public class FormFieldEditorClass extends AbstractClassDefinition implements Str
   public static final String DOC_NAME = "FormFieldEditorClass";
   public static final String CLASS_DEF_HINT = SPACE_NAME + "." + DOC_NAME;
 
+  public enum Method {
+    GET, POST;
+  }
+
   public static final ClassField<String> FIELD_ACTION = new StringField.Builder(CLASS_DEF_HINT,
       "form_field_action").prettyName("Action").build();
-  public static final ClassField<String> FIELD_METHOD = new StringField.Builder(CLASS_DEF_HINT,
-      "form_field_method").prettyName("Method").build();
+  public static final ClassField<List<Method>> FIELD_METHOD = new EnumListField.Builder<>(
+      CLASS_DEF_HINT, "form_field_method", Method.class).prettyName("Method").multiSelect(
+          false).build();
   public static final ClassField<Boolean> FIELD_SEND_DATA_ENCODED = new BooleanField.Builder(
       CLASS_DEF_HINT, "form_field_send_data_encoded").prettyName("Send Data Encoded").build();
   public static final ClassField<String> FIELD_PREFIX = new StringField.Builder(CLASS_DEF_HINT,
