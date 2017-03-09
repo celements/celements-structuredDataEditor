2d263ef720231b169ffb14fe77fea848cbb461e7
Gal-Cohen08 <galcohen08@gmail.com>
2017-03-09 18:05:47 +0100

    DefaultStructuredDataEditorService

--- component/src/test/java/com/celements/structEditor/DefaultStructuredDataEditorServiceTest.java
+++ component/src/test/java/com/celements/structEditor/DefaultStructuredDataEditorServiceTest.java
@@ -24,6 +24,7 @@ import com.celements.structEditor.fields.FormFieldPageType;
 import com.google.common.base.Optional;
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
 import com.xpn.xwiki.objects.classes.BaseClass;
 import com.xpn.xwiki.web.Utils;
 
@@ -76,6 +77,22 @@ public class DefaultStructuredDataEditorServiceTest extends AbstractComponentTes
     verifyDefault();
     assertEquals(prefix, ret);
   }
+
+  // @Test
+  // public void test_resolveFormPrefix_DocumentNotExists() throws Exception {
+  // String prefix = null;
+  // XWikiDocument parentDoc = new XWikiDocument(new DocumentReference(wikiName, "layout",
+  // "parent"));
+  // DocumentReference xClassDocRef = new DocumentReference(wikiName, "Celements",
+  // "TestXClassName");
+  // expect(modelAccessMock.getDocument(xClassDocRef)).andThrow(new DocumentNotExistsException(
+  // xClassDocRef)).once();
+
+  // replayDefault();
+  // String ret = service.resolveFormPrefix(parentDoc);
+  // verifyDefault();
+  // assertEquals(prefix, ret);
+  // }
 
   @Test
   public void test_getXClassPrettyName() throws Exception {
@@ -161,9 +178,6 @@ public class DefaultStructuredDataEditorServiceTest extends AbstractComponentTes
     XWikiDocument cellDoc = new XWikiDocument(new DocumentReference("wikiName", "Celements",
         "cell"));
     XWikiDocument onDoc = new XWikiDocument(new DocumentReference("wikiName", "Celements", "on"));
-    // BaseObject obj = new BaseObject();
-    // obj.setXClassReference(docRef2);
-    // onDoc.addXObject(obj);
     expect(modelAccessMock.getProperty(cellDoc,
         StructuredDataEditorClass.FIELD_EDIT_FIELD_CLASS_NAME)).andReturn(modelutils.serializeRef(
             classRef)).once();
@@ -182,6 +196,113 @@ public class DefaultStructuredDataEditorServiceTest extends AbstractComponentTes
     name = service.getAttributeNameInternal(cellDoc, onDoc);
     verifyDefault();
     assertEquals(ostr, name);
+
+  }
+
+  @Test
+  public void test_getAttributeNameInternal_fieldName_classRef_BaseObjectNotNull()
+      throws Exception {
+    DocumentReference classRef = new DocumentReference("wikiName", "Celements", "TestXClassName");
+    XWikiDocument cellDoc = new XWikiDocument(new DocumentReference("wikiName", "Celements",
+        "cell"));
+    XWikiDocument onDoc = new XWikiDocument(new DocumentReference("wikiName", "Celements", "on"));
+    BaseObject obj = new BaseObject();
+    obj.setXClassReference(classRef);
+    obj.setNumber(7);
+    expect(modelAccessMock.getProperty(cellDoc,
+        StructuredDataEditorClass.FIELD_EDIT_FIELD_CLASS_NAME)).andReturn(modelutils.serializeRef(
+            classRef)).once();
+    expect(modelAccessMock.getProperty(cellDoc,
+        StructuredDataEditorClass.FIELD_EDIT_FIELD_NAME)).andReturn("Test").times(2);
+    expect(modelAccessMock.getXObject(onDoc, classRef)).andReturn(obj);
+    List<String> nameParts = new ArrayList<>();
+    nameParts.add("7");
+    String testString1 = "TestXClassName";
+    String testString2 = "Test";
+    Optional<String> ostr = Optional.fromNullable("Celements." + testString1 + "_" + nameParts.get(
+        0) + "_" + testString2);
+    Optional<String> name;
+
+    replayDefault();
+    name = service.getAttributeNameInternal(cellDoc, onDoc);
+    verifyDefault();
+    assertEquals(ostr, name);
+
+  }
+
+  @Test
+  public void test_getDateFormatFromField_classRefNotPresent_fieldNameNotPresent()
+      throws Exception {
+    DocumentReference classRef = new DocumentReference("wikiName", "Celements", "TestXClassName");
+    XWikiDocument cellDoc = new XWikiDocument(new DocumentReference("wikiName", "Celements",
+        "cell"));
+    expect(modelAccessMock.getDocument(classRef)).andReturn(cellDoc);
+    expect(modelAccessMock.getProperty(cellDoc,
+        StructuredDataEditorClass.FIELD_EDIT_FIELD_CLASS_NAME)).andReturn(null).times(1);
+    expect(modelAccessMock.getProperty(cellDoc,
+        StructuredDataEditorClass.FIELD_EDIT_FIELD_NAME)).andReturn(null).times(1);
+
+    Optional<String> name;
+    replayDefault();
+    name = service.getDateFormatFromField(classRef);
+    verifyDefault();
+
+    assertEquals(Optional.absent(), name);
+
+  }
+
+  @Test
+  public void test_getDateFormatFromField_classRefPresent_fieldNameNotPresent() throws Exception {
+    DocumentReference classRef = new DocumentReference("wikiName", "Celements", "TestXClassName");
+    XWikiDocument cellDoc = new XWikiDocument(new DocumentReference("wikiName", "Celements",
+        "cell"));
+    expect(modelAccessMock.getProperty(cellDoc,
+        StructuredDataEditorClass.FIELD_EDIT_FIELD_CLASS_NAME)).andReturn(modelutils.serializeRef(
+            classRef)).times(1);
+    expect(modelAccessMock.getProperty(cellDoc,
+        StructuredDataEditorClass.FIELD_EDIT_FIELD_NAME)).andReturn("fieldTest").times(1);
+    expect(modelAccessMock.getDocument(classRef)).andReturn(cellDoc).times(2);
+    BaseClass xClass = cellDoc.getXClass();
+    String thePrettyFieldName = "the Pretty Field Name";
+    xClass.addDateField("fieldTest", thePrettyFieldName, "dd.MM.yyyy");
+    replayDefault();
+    Optional<String> name = service.getDateFormatFromField(classRef);
+    verifyDefault();
+    assertEquals("dd.MM.yyyy", name.get());
+  }
+
+  @Test
+  public void test_getCellValueAsString_valueIsNull() throws Exception {
+    DocumentReference celldocRef = new DocumentReference("wikiName", "Celements", "TestXClassName");
+    XWikiDocument indoc = new XWikiDocument(new DocumentReference("wikiName", "Celements", "cell"));
+    XWikiDocument ondoc = new XWikiDocument(new DocumentReference("wikiName1", "Celements1",
+        "cell1"));
+    expect(modelAccessMock.getDocument(celldocRef)).andReturn(indoc);
+    expect(modelAccessMock.getProperty(eq(indoc), eq(
+        StructuredDataEditorClass.FIELD_EDIT_FIELD_NAME))).andReturn(null);
+    replayDefault();
+    Optional<String> name = service.getCellValueAsString(celldocRef, ondoc);
+    verifyDefault();
+    assertEquals(Optional.absent(), name);
+  }
+
+  @Test
+  public void test_getCellValueAsString_valueNotNull() throws Exception {
+
+    DocumentReference celldocRef = new DocumentReference("wikiName", "Celements", "TestXClassName");
+    XWikiDocument indoc = new XWikiDocument(new DocumentReference("wikiName", "Celements", "cell"));
+    XWikiDocument ondoc = new XWikiDocument(new DocumentReference("wikiName1", "Celements1",
+        "cell1"));
+    expect(modelAccessMock.getDocument(celldocRef)).andReturn(indoc);
+    expect(modelAccessMock.getProperty(eq(indoc), eq(
+        StructuredDataEditorClass.FIELD_EDIT_FIELD_NAME))).andReturn("fieldName");
+    expect(modelAccessMock.getProperty(eq(indoc), eq(
+        StructuredDataEditorClass.FIELD_EDIT_FIELD_CLASS_NAME))).andReturn("fieldname");
+    expect(modelAccessMock.getProperty(ondoc, classRef, name))
+    replayDefault();
+    Optional<String> name = service.getCellValueAsString(celldocRef, ondoc);
+    verifyDefault();
+    assertEquals(Optional.of("val"), name);
 
   }
 }
