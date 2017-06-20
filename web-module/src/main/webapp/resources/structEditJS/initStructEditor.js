2398c95146ab0024e761353adc9b617c5e1b980d
Fabian <fabian.pichler@synventis.com>
2017-06-20 17:10:40 +0200

    Struct Editor: Implement javascript editor framework  (#15)
    * add struct editor manager and initializer
    
    * change version to 1.4-CELDEV-426-SNAPSHOT
    
    * move to resources dir
    
    * fix StructEditorManager class definition
    
    * add javascript note
    
    * fix class creation
    
    * fix StructEditor class creation
    
    * add isDirty todo
    
    * implement checkBeforeUnload and isDirty
    
    * add missing if expression
    
    * add js doc
    
    * fix syntax _allStructEditorMap access
    
    * add missing structRootId computation
    
    * replace newEditor with _me in isDirty
    
    * replace add with or in isDirty computation
    
    * spelling and syntax
    
    * fix syntax in _formDirtyOnLoad
    
    * add checkUnsavedChanges to editor manager
    
    * add saving functions
    
    * replace _me.formElem with _me._formElem in isDirty
    
    * fix syntax in saveAllFormsAsync
    
    * add StructEditorMessages.vm to Messages.vm
    
    * add yahoo css
    
    * increase zindex for dialog to 101
    
    * add structEditorModalDialog css class
    
    * add css for struct editor dialog
    
    * add yui js in comment
    
    * add close button handling
    
    * add missing _me definition in _closeClickHandler
    
    * add stop.event to closeClickHandler
    
    * add logging
    
    * fix getCancelURL
    
    * add save button handler
    
    * add 'event' to parameter list of saveClickHandler
    
    * fix saveClickHandler
    
    * remove missing transport parameter in click handlers callbacks
    
    * add logging
    
    * add saveAndContinue
    
    * add logging
    
    * remove 'transport' from callback parameter list in saveAndContinue
    
    * refactoring saveAndContinue callback check
    
    * add save ajax handling
    
    * fix spelling of getDirtyFormIds
    
    * add doNotSaveFormId to saveAllDirtyForms
    
    * remove transport parameter from saveAllDirtyForms callback
    
    * add missing _saveAndContinueAjax
    
    * source code formatting
    
    * add loaded css classname
    
    * add trigger css class celementsCheckUnsaved for forms to load
    StructEditor
    
    * update version to 1.4-SNAPSHOT
    
    * refactoring: extract UrlFactory class
    
    * add missing CELEMENTS.Utils
    
    * fix spelling in 'Utils.UrlFactory'
    
    * refactoring extract CELEMENTS.structEdit.CelementsFormSaver class
    
    * refactoring: extract UnsavedChangesHandler class
    
    * fix call to URLFactory constructor
    
    * fix UnsavedChangesHandler constructor
    
    * add return value to _getUnsavedChangesHandler
    
    * added missing _me definitions to methods in CelementsFormSaver
    
    * add logging
    
    * add logging
    
    * fix callback call in saveAllForms
    
    * add missing NL before EOF
    
    * add missing NL before EOF
    
    * remove empty comment
    
    * correct package name
    
    * add JIRA issue number to TODO comment
    
    * add documentation to _showErrorMessages
    
    * change fixme to comment because it is not fixable
    
    * add handling of onFailure saving editors
    
    * add on302
    
    * add onError handling
    
    * add logging
    
    * fix param list for _getSavingFailedJson
    
    * fix jsonResponse naming in _handleSaveAjaxJsonResponse
    
    * add multilingual support for saved failed dialog
    
    * remove debugging

---
+++ web-module/src/main/webapp/resources/structEditJS/initStructEditor.js
@@ -0,0 +1,43 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+(function(window, undefined) {
+  "use strict";
+
+  /**
+   * Initialize struct editors and register to initialize on contentChanged events.
+   */
+
+  var initStructEditorContentChangedHandler = function(event) {
+    var _me = this;
+    var checkRoot = event.memo.htmlElem || document.body;
+    console.log('initStructEditorContentChangedHandler: run for ', checkRoot);
+    $(document.body).fire('cel:initStructEditor', { 'checkRoot' : checkRoot });
+    console.log('initStructEditorContentChangedHandler: finish for ', checkRoot);
+  };
+
+  celAddOnBeforeLoadListener(function() {
+    window.celStructEditorManager = new CELEMENTS.structEdit.StructEditorManager();
+    $(document.body).stopObserving("celements:contentChanged", initStructEditorContentChangedHandler);
+    $(document.body).observe("celements:contentChanged", initStructEditorContentChangedHandler);
+    window.celStructEditorManager.startEditorManager();
+  });
+
+})(window);
