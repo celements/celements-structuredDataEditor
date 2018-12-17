14d510c8d7ded215b290ac3aa7835c5072630694
Marc Sladek <marc.sladek@synventis.com>
2018-12-17 19:57:30 +0100

    add unit tests

---
+++ component/src/test/java/com/celements/struct/table/TableRowPresentationTypeTest.java
@@ -0,0 +1,98 @@
+package com.celements.struct.table;
+
+import static com.celements.common.test.CelementsTestUtils.*;
+import static org.easymock.EasyMock.*;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.velocity.VelocityManager;
+
+import com.celements.common.test.AbstractComponentTest;
+import com.celements.model.access.IModelAccessFacade;
+import com.celements.navigation.presentation.IPresentationTypeRole;
+import com.celements.pagetype.PageTypeReference;
+import com.celements.pagetype.service.IPageTypeResolverRole;
+import com.celements.search.lucene.ILuceneSearchService;
+import com.celements.web.service.IWebUtilsService;
+import com.google.common.base.Optional;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.web.Utils;
+
+public class TableRowPresentationTypeTest extends AbstractComponentTest {
+
+  private TableRowPresentationType presentationType;
+  private XWikiDocument doc;
+
+  @Before
+  public void prepareTest() throws Exception {
+    registerComponentMocks(IModelAccessFacade.class, IWebUtilsService.class,
+        IPageTypeResolverRole.class, ILuceneSearchService.class, VelocityManager.class);
+    presentationType = (TableRowPresentationType) Utils.getComponent(IPresentationTypeRole.class,
+        TableRowPresentationType.NAME);
+    doc = new XWikiDocument(new DocumentReference("xwikidb", "space", "tabledoc"));
+    getContext().setDoc(doc);
+  }
+
+  @Test
+  public void test_resolveMacroName() {
+    TableConfig table = getDummyTableConfig();
+    ColumnConfig col = table.getColumns().get(0);
+    col.setTitle("The Name");
+    expectPageTypeRef("tableName");
+
+    replayDefault();
+    assertEquals("celStruct/table/tableName/col_the_name.vm", presentationType.resolveMacroName(
+        col));
+    verifyDefault();
+  }
+
+  @Test
+  public void test_resolveMacroName_fallback_1() {
+    TableConfig table = getDummyTableConfig();
+    table.setCssId("tableName");
+    ColumnConfig col = table.getColumns().get(0);
+    col.setOrder(5);
+    expectAbsentPageTypeRef();
+
+    replayDefault();
+    assertEquals("celStruct/table/tableName/col_5.vm", presentationType.resolveMacroName(col));
+    verifyDefault();
+  }
+
+  @Test
+  public void test_resolveMacroName_fallback_2() {
+    TableConfig table = getDummyTableConfig();
+    ColumnConfig col = table.getColumns().get(0);
+    expectAbsentPageTypeRef();
+
+    replayDefault();
+    assertEquals("celStruct/table/tabledoc/col_2.vm", presentationType.resolveMacroName(col));
+    verifyDefault();
+  }
+
+  private TableConfig getDummyTableConfig() {
+    TableConfig table = new TableConfig();
+    table.setDocumentReference(doc.getDocumentReference());
+    ColumnConfig col = new ColumnConfig();
+    col.setNumber(2);
+    table.setColumns(Arrays.asList(col));
+    return table;
+  }
+
+  private void expectAbsentPageTypeRef() {
+    expect(getMock(IPageTypeResolverRole.class).resolvePageTypeReference(same(doc))).andReturn(
+        Optional.<PageTypeReference>absent());
+  }
+
+  private void expectPageTypeRef(String configName) {
+    expect(getMock(IPageTypeResolverRole.class).resolvePageTypeReference(same(doc))).andReturn(
+        Optional.fromNullable(new PageTypeReference(configName, null,
+            Collections.<String>emptyList())));
+  }
+
+}
