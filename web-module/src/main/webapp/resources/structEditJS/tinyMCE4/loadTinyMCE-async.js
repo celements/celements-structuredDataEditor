9092929b61674d34cbcfb36dd3f1ca6fef10b313
Fabian <fabian.pichler@synventis.com>
2019-06-06 21:22:56 +0200

    CELDEV-823 - add tinyMCE compat3x and celimage, cellink (#57) (#58)
    * update component dependency to SNAPSHOT
    
    * add loadTinyMCE-async, move config to server
    
    * syntax error in initStructEditor.js
    
    * add compat3x and loadTinyMCE-async js
    
    * fix start timing issue
    
    * fix inline usage of loadTinyMCE-async
    
    * fix tinyMCE start and config
    
    * fix JSON syntax
    
    * rename Tiny4Config
    
    * improve tiny config
    
    * change to 3-JSON builder
    
    * remove old body_class hack.
    
    * hide tiny menubar
    
    * add missing definition of tinyConfigJSON
    
    * removed old tinyMCE startup
    
    * add plugins to tinyConfig4
    
    * rename celimage and cellink
    
    * add external_plugins for 3x plugins
    
    * fix plugin url computation
    
    * add toolbar1 config
    
    * fix json syntax
    
    * add tiny config values
    
    * add width
    
    * remove duplicate content_css
    
    * add filebase params

---
+++ web-module/src/main/webapp/resources/structEditJS/tinyMCE4/loadTinyMCE-async.js
@@ -0,0 +1,70 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+(function(window, undefined) {
+  "use strict";
+  
+  var initCelRTE4 = function() {
+    console.log('initCelRTE4: start');
+    var params = {
+        xpage : 'celements_ajax',
+        ajax_mode : 'struct/Tiny4Config'
+     };
+    var hrefSearch = window.location.search;
+    var templateRegEx = new RegExp('^(\\?|(.*&)+)?template=([^=&]*).*$');
+    if (hrefSearch.match(templateRegEx)) {
+      params['template'] = window.location.search.replace(templateRegEx, '$3');
+    }
+    console.log('initCelRTE4: before Ajax tinymce');
+    new Ajax.Request(getCelHost(), {
+      method: 'post',
+      parameters: params,
+      onSuccess: function(transport) {
+        var tinyConfigJSON = transport.responseText;
+        console.log('tinyMCE4 config loaded: starting tiny');
+        if (tinyConfigJSON.isJSON()) {
+          var tinyConfigObj = tinyConfigJSON.evalJSON();
+          console.log('initCelRTE4: tinyMCE.init');
+          tinyMCE.init(tinyConfigObj);
+        } else {
+          console.error('TinyConfig is no json!', tinyConfigJSON);
+        }
+      }
+    });
+  };
+  
+  (function(structManager){
+    console.log('loadTinyMCE async: start');
+    if (structManager) {
+      if (!structManager.isStartFinished()) {
+        console.log('structEditorManager not initialized: register for finishLoading');
+        structManager.celStopObserving('structEdit:finishedLoading', initCelRTE4);
+        structManager.celObserve('structEdit:finishedLoading', initCelRTE4);
+      } else {
+        console.log('structEditorManager already initialized: initCelRTE4');
+        initCelRTE4();
+      }
+    } else {
+      console.error('No struct editor manager found -> Failed to initialize tinyMCE4.');
+    }
+    console.log('loadTinyMCE async: end');
+  })(window.celStructEditorManager);
+  
+})(window);
