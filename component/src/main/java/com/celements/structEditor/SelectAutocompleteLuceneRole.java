dee0c4f43a5210838189ccb90074fde41d33cc3b
Marc Sladek <marc@sladek.me>
2021-01-19 16:38:17 +0100

    add @ComponentRole

--- component/src/main/java/com/celements/structEditor/SelectAutocompleteLuceneRole.java
+++ component/src/main/java/com/celements/structEditor/SelectAutocompleteLuceneRole.java
@@ -3,8 +3,11 @@ package com.celements.structEditor;
 import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 
+import org.xwiki.component.annotation.ComponentRole;
+
 import com.celements.search.lucene.LuceneSearchResult;
 
+@ComponentRole
 public interface SelectAutocompleteLuceneRole extends SelectAutocompleteRole {
 
   @NotNull
