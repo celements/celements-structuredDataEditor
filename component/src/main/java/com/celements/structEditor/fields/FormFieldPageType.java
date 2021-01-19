69f632bbeb22fab7a4e1918e9089a215ba1f5eef
Marc Sladek <marc@sladek.me>
2021-01-19 16:16:14 +0100

    add form autocomplete off

--- component/src/main/java/com/celements/structEditor/fields/FormFieldPageType.java
+++ component/src/main/java/com/celements/structEditor/fields/FormFieldPageType.java
@@ -1,8 +1,9 @@
 package com.celements.structEditor.fields;
 
 import static com.celements.structEditor.classes.FormFieldEditorClass.*;
+import static java.lang.Boolean.*;
 
-import java.util.Collections;
+import java.util.Collection;
 import java.util.Optional;
 
 import org.xwiki.component.annotation.Component;
@@ -11,8 +12,8 @@ import org.xwiki.velocity.XWikiVelocityException;
 
 import com.celements.cells.attribute.AttributeBuilder;
 import com.celements.model.access.exception.DocumentNotExistsException;
+import com.celements.model.object.xwiki.XWikiObjectFetcher;
 import com.celements.structEditor.classes.FormFieldEditorClass.Method;
-import com.google.common.collect.Iterables;
 import com.xpn.xwiki.doc.XWikiDocument;
 
 @Component(FormFieldPageType.PAGETYPE_NAME)
@@ -42,13 +43,17 @@ public class FormFieldPageType extends AbstractStructFieldPageType {
     attrBuilder.addCssClasses("celAddValidationToForm inactive");
     try {
       XWikiDocument cellDoc = modelAccess.getDocument(cellDocRef);
-      if (modelAccess.getFieldValue(cellDoc, FIELD_SEND_DATA_ENCODED).or(true)) {
+      XWikiObjectFetcher fetcher = XWikiObjectFetcher.on(cellDoc);
+      if (fetcher.fetchField(FIELD_SEND_DATA_ENCODED).stream().anyMatch(TRUE::equals)) {
         attrBuilder.addNonEmptyAttribute("enctype", "multipart/form-data");
       }
       attrBuilder.addNonEmptyAttribute("action", getVelocityFieldValue(cellDoc, FIELD_ACTION)
           .orElse("?"));
-      attrBuilder.addNonEmptyAttribute("method", Iterables.getFirst(modelAccess.getFieldValue(
-          cellDoc, FIELD_METHOD).or(Collections.<Method>emptyList()), Method.POST).name());
+      attrBuilder.addNonEmptyAttribute("method", fetcher.fetchField(FIELD_METHOD)
+          .stream().flatMap(Collection::stream)
+          .findFirst().orElse(Method.POST)
+          .name());
+      attrBuilder.addNonEmptyAttribute("autocomplete", "off");
     } catch (DocumentNotExistsException | XWikiVelocityException exc) {
       log.error("failed to add all attributes for '{}'", cellDocRef, exc);
     }
