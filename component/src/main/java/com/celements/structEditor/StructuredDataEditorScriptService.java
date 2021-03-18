1bc763b1f8fd4ecd1f49b298862154f73bf88637
Marc Sladek <marc@sladek.me>
2021-03-18 01:59:35 +0100

    Autocomplete with type

--- component/src/main/java/com/celements/structEditor/StructuredDataEditorScriptService.java
+++ component/src/main/java/com/celements/structEditor/StructuredDataEditorScriptService.java
@@ -11,6 +11,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.Requirement;
+import org.xwiki.component.manager.ComponentLookupException;
+import org.xwiki.component.manager.ComponentManager;
 import org.xwiki.model.reference.ClassReference;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.script.service.ScriptService;
@@ -49,6 +51,9 @@ public class StructuredDataEditorScriptService implements ScriptService {
 
   @Requirement
   private ModelContext context;
+
+  @Requirement
+  private ComponentManager componentManager;
 
   public String getAttributeName(DocumentReference cellDocRef) {
     return getAttributeName(cellDocRef, null);
@@ -154,6 +159,14 @@ public class StructuredDataEditorScriptService implements ScriptService {
   public Optional<SelectAutocompleteRole> getSelectTagAutoCompleteImpl(
       DocumentReference cellDocRef) {
     return selectTagService.getTypeImpl(cellDocRef);
+  }
+
+  public Optional<SelectAutocompleteRole> getSelectTagAutoCompleteImpl(String name) {
+    try {
+      return Optional.of(componentManager.lookup(SelectAutocompleteRole.class, name));
+    } catch (ComponentLookupException exc) {
+      return Optional.empty();
+    }
   }
 
   public boolean isMultilingual(DocumentReference cellDocRef) {
--- web-module/src/main/webapp/resources/structEditJS/autocomplete.js
+++ web-module/src/main/webapp/resources/structEditJS/autocomplete.js
@@ -47,8 +47,7 @@
     document.querySelectorAll('.structAutocomplete').forEach(selectElem => {
       console.log('initAutocomplete:', selectElem, language)
       const type = selectElem.dataset.autocompleteType || '';
-      const cellRef = selectElem.dataset.cellRef || '';
-      $j(selectElem).select2(buildSelect2Config(type, language, cellRef))
+      $j(selectElem).select2(buildSelect2Config(type, language))
       $j(selectElem).on('select2:unselect', clearSelectOptions);
     });
   };
@@ -62,7 +61,8 @@
   const processResults = function (response, params) {
     params.page = params.page || 1;
     return {
-      results: response.results.map(elem => {
+      results: (response.results || [])
+        .map(elem => {
           elem.id = elem.fullName;
           elem.text = elem.name;
           return elem;
@@ -91,14 +91,14 @@
     selectElem.innerHTML = '<option selected="selected" value="">delete</option>';
   };
 
-  const buildSelect2Config = function(type, language, cellRef) {
+  const buildSelect2Config = function(type, language) {
     return {
       language: language,
       placeholder: celMessages.progMsg.select2["cel_select2_autocomplete_placeholder_" + type],
       allowClear: true,
       selectionCssClass: "structSelectContainer " + type + "SelectContainer",
       dropdownCssClass: "structSelectDropDown " + type + "SelectDropDown",
-      ajax: buildSelect2Request(cellRef),
+      ajax: buildSelect2Request(type),
       escapeMarkup: function (markup) {
         // default Utils.escapeMarkup is HTML-escaping the value. Because
         // we formated the value using HTML it must not be further escaped.
@@ -110,7 +110,7 @@
     };
   };
 
-  const buildSelect2Request = function(cellRef, limit = 10) {
+  const buildSelect2Request = function(type, limit = 10) {
     return {
       url: "/OrgExport/REST",
       dataType: 'json',
@@ -125,7 +125,7 @@
           'ajax' : 1,
           'xpage' : 'celements_ajax',
           'ajax_mode' : 'struct/autocomplete/search',
-          'cellRef' : cellRef,
+          'type' : type,
           'searchterm' : params.term,
           'page' : page,
           'limit' : limit,
